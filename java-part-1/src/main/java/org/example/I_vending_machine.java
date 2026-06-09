4adce36995b71cbfe782a8025e641546a6dcff2b
Ascody <actaney@gmail.com>
2026-06-09 15:25:00 +0900

    day 4

--- java-part-1/src/main/java/org/example/I_vending_machine.java
+++ java-part-1/src/main/java/org/example/I_vending_machine.java
@@ -12,30 +12,30 @@ public class I_vending_machine {
 
     static final int COKE = 500, CIDER = 700, FANTA = 300, WATER = 200;
 
-    public static void printMenu(int totalMoney){
+    static void printMenu(int totalMoney){
         System.out.println("=============================== 자판기 ==============================");
         System.out.println("[1]콜라-500원 [2]사이다-700원 [3]환타-300원 [4]물-200원 [5]돈넣기 [6]종료");
         System.out.println("현재 금액 : " + totalMoney + "원");
         System.out.println("======================================================================");
     }
 
-    public static int getChoice(){
+    static int getChoice(){
         Scanner sc = new Scanner(System.in);
         System.out.println("원하는 메뉴를 선택하세요.");
         return sc.nextInt();
     }
 
-    public static int getMoney(){
+    static int getMoney(){
         Scanner sc = new Scanner(System.in);
         System.out.println("돈을 넣으세요.");
         return sc.nextInt();
     }
 
-    public static int calcMoney(int totalMoney, int price){
+    static int calcMoney(int totalMoney, int price){
         return totalMoney - price;
     }
 
-    public static void calcMoneyException() {
+    static void calcMoneyException() {
         System.out.println("잔돈이 부족합니다.");
     }
 
